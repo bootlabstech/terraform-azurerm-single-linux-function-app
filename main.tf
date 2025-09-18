a38f8b061c7df9c61449d4114143c814a998a6ac
lalithaa2407 <lalitalallu02@gmail.com>
2025-09-18 13:56:28 +0530

    linux functionapp single 4 provider

---
+++ main.tf
@@ -0,0 +1,56 @@
+
+
+
+resource "azurerm_linux_function_app" "example" {
+  name                = var.name
+  resource_group_name = var.resource_group_name
+  location            = var.location
+  virtual_network_subnet_id = var.virtual_network_subnet_id
+
+  storage_account_name       = var.storage_accountid
+  service_plan_id            = var.app_service_planid
+  
+  site_config {
+    ftps_state             = var.ftps_state
+    app_command_line       = var.app_command_line
+    app_scale_limit        = var.app_scale_limit
+    vnet_route_all_enabled = var.vnet_route_all_enabled
+  }
+
+  depends_on = [ azurerm_app_service_plan.app_service_plan ]
+
+  
+}
+# resource "azurerm_storage_account" "storage_account" {
+#   name                          = var.storage_name
+#   resource_group_name           = var.resource_group_name
+#   location                      = var.location
+#   account_tier                  = var.account_tier
+#   account_replication_type      = var.account_replication_type
+#   public_network_access_enabled = var.public_network_access_enabled
+
+#   lifecycle {
+#     ignore_changes = [
+#       tags,
+#     ]
+#   }
+# }
+
+# resource "azurerm_app_service_plan" "app_service_plan" {
+#   name                = var.plan_name
+#   resource_group_name = var.resource_group_name
+#   location            = var.location
+#   kind                = var.kind
+#   reserved            = var.reserved
+
+#   sku {
+#     tier = var.tier
+#     size = var.size
+#   }
+
+#   lifecycle {
+#     ignore_changes = [
+#       tags,
+#     ]
+#   }
+# }
---
+++ variables.tf
@@ -0,0 +1,87 @@
+variable "resource_group_name" {
+  description = "Name of the resource group"
+  type        = string
+}
+variable "location" {
+  description = "The Azure Region where the Resource Group should exist."
+  type        = string
+}
+
+# # STORAGE ACCOUNT
+
+# variable "storage_name" {
+#   description = "Specifies the name of the storage account."
+#   type        = string
+# }
+# variable "account_tier" {
+#   description = "Defines the Tier to use for this storage account."
+#   type        = string
+#   default     = "Standard"
+# }
+# variable "account_replication_type" {
+#   description = "Defines the type of replication to use for this storage account."
+#   type        = string
+#   default     = "LRS"
+# }
+
+# # APP SERVICE PLAN
+
+# variable "plan_name" {
+#   description = "Specifies the name of the App Service Plan component."
+#   type        = string
+# }
+# variable "tier" {
+#   description = "Specifies the plan's pricing tier."
+#   type        = string
+# }
+# variable "size" {
+#   description = "Specifies the plan's instance size."
+#   type        = string
+# }
+variable "app_service_planid" {
+  type        = string
+  description = "The kind of the App Service Plan to create. Possible values are Windows (also available as App), Linux, elastic (for Premium Consumption) and FunctionApp (for a Consumption Plan). Defaults to Windows."
+
+}
+variable "storage_accountid" {
+  type        = string
+  description = "Is this App Service Plan Reserved. Defaults to false."
+
+}
+
+# FUNCTION APP
+
+variable "name" {
+  description = "the name of the Function App."
+  type        = string
+}
+
+
+variable "ftps_state" {
+  type    = string
+  default = "FtpsOnly"
+
+}
+variable "app_command_line" {
+  type    = string
+  default = ""
+
+}
+variable "app_scale_limit" {
+  type    = number
+  default = 1
+
+}
+variable "vnet_route_all_enabled" {
+  type    = bool
+  default = false
+
+}
+variable "public_network_access_enabled" {
+  type = bool
+  default = true
+}
+variable "virtual_network_subnet_id" {
+  type = string
+  
+}
